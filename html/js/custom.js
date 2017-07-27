3d267639d5d56ab640f466bb8779de410aa77d3b
Sudipta Mallick <sudipta@capitalnumbers.com>
2017-07-27 20:34:05 +0530

    parallax effect edited, portfolio slider is in progress.

--- html/js/custom.js
+++ html/js/custom.js
@@ -1,3 +1,10 @@
-jQuery(document).ready(function(){
-    jQuery('.parallax').parallax();
-});
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.carousel').carousel({fullWidth: true});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
