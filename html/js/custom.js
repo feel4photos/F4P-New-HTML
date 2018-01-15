38424105868ef12450f633b295872cfa2c35f2d8
Samhita-Sudipta <Samhita-Sudipta@Macbooks-MacBook-Pro.local>
2018-01-15 21:35:49 +0530

    sticky header is added.

--- html/js/custom.js
+++ html/js/custom.js
@@ -157,7 +157,10 @@ jQuery(window).resize(function(){
 // grab an element
 var myElement = document.querySelector("section.o-brand");
 // construct an instance of Headroom, passing the element
-var headroom  = new Headroom(myElement);
+var headroom  = new Headroom(myElement, {
+    "offset": 10,
+    "tolerance": 10
+});
 // initialise
 headroom.init();
 
